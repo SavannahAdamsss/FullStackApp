fff0f26960c13bcfb0e3ff0e73c75fa2c0cfc8f4
SavannahAdamsss <savannah.adams0@gmail.com>
2018-06-18 16:35:26 -0600

    Adding production database

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,8 +5,12 @@ module.exports = {
   development: {
     client: "pg",
     connection: `postgres:///${process.env.LOCAL_DATABASE_NAME}`
-    }
-  };
+    },
+  production: {
+    client: "pg",
+    connection: `${process.env.DATABASE_URL}?ssl=true`
+  }
+};
 
   // staging: {
   //   client: 'postgresql',
